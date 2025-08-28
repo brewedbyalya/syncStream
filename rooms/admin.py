d008d5d5896f7ec2d1b0cad62e1bbe5028cc31e9
alya <aliya2002mah@gmail.com>
2025-08-28 14:30:48 +0300

    Worked on templates. CSS is ai generated for now.

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Room, Participant, Message, ScreenSession
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'created_at', 'is_private', 'is_active')
+    list_filter = ('is_private', 'is_active', 'created_at')
+    search_fields = ('name', 'creator__username')
+    readonly_fields = ('created_at',)
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'joined_at', 'is_online')
+    list_filter = ('is_online', 'joined_at')
+    search_fields = ('user__username', 'room__name')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'message_type', 'created_at')
+    list_filter = ('message_type', 'created_at')
+    search_fields = ('user__username', 'room__name', 'message')
+    readonly_fields = ('created_at',)
+
+@admin.register(ScreenSession)
+class ScreenSessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'started_at', 'ended_at', 'is_active')
+    list_filter = ('is_active', 'started_at')
+    search_fields = ('user__username', 'room__name')
+    readonly_fields = ('started_at', 'ended_at')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_online', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_online', 'last_login')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('profile_picture', 'bio', 'is_online')}),
+    )
