e0f0cb38f80ce38ad843060b5718fcbda47b14fe
alya <aliya2002mah@gmail.com>
2025-09-02 21:57:01 +0300

    ,

--- syncstream_project/settings.py
+++ syncstream_project/settings.py
@@ -81,8 +81,8 @@ if DATABASE_URL:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "syncstream",
         }
     }
 
